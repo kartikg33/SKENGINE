379301a707fe95c25ac2e02cabbdcdceedc7b817
Kartik Gohil <kartik_gohil@hotmail.com>
2015-09-21 19:47:22 +0100

    wrote a view to pull Text objects from the database and output their text values to the web page

--- skengine/skengine/urls.py
+++ skengine/skengine/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^skengine_app/', include('skengine_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- skengine/skengine_app/views.py
+++ skengine/skengine_app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
+from .models import Text, Image, Video, Link
+
+def editSketch(request):
+    output = [p.txt for p in Text.objects.all()]
+    return HttpResponse(output)
