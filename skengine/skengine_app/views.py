c2f1a4f54503c62f4aca9407f0d35853868f04b0
Kartik Gohil <kartik_gohil@hotmail.com>
2015-09-21 20:00:17 +0100

    made template editSketch.html to display html content on screen

--- skengine/skengine_app/views.py
+++ skengine/skengine_app/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from .models import Text, Image, Video, Link
 
 def editSketch(request):
-    output = [p.txt for p in Text.objects.all()]
-    return HttpResponse(output)
+    text_list = Text.objects.all()
+    context = {'text_list': text_list}
+    return render(request, 'skengine_app/editSketch.html', context)
+
+    """output = [p.txt for p in Text.objects.all()]
+    return HttpResponse(output)"""
