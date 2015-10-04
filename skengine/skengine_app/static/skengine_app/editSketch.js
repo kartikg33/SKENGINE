efd29a0ea0cbfc0adc5599ec9bfd9db09b3c26a0
Kartik Gohil <kartik_gohil@hotmail.com>
2015-10-04 15:56:02 +0100

    no need for global dragging variable. dragging works off the 'selected' class

--- skengine/skengine_app/static/skengine_app/editSketch.js
+++ skengine/skengine_app/static/skengine_app/editSketch.js
@@ -86,12 +86,13 @@ $(document).ready(function(){
 			jQuery("<div/>", {
 		    	id: numFrames
 			}).appendTo("body");
-			//$("#"+numFrames).load("box.htm"); // Loads HTML for New Frame
 			$("#"+numFrames).html('<div class = "container txt"><p>Enter Text</p></div>');
 			var newFrame = $("#"+numFrames);
+
 			//Calculate Position of New Frame
 			var left = currentMousePos.x-100;
 			var top = currentMousePos.y-100;
+
 			//Calculate Position from Centre
 			var posX = left - screen_centre.x + window_pos.x;
 			var posY = top - screen_centre.y + window_pos.y;
@@ -99,6 +100,7 @@ $(document).ready(function(){
 				'data-centre-x': posX,
 				'data-centre-y': posY
 			});
+
 			//Set Position of New Frame
 			newFrame.css({
 				'width': '200px',
@@ -107,95 +109,50 @@ $(document).ready(function(){
 				'top': 	top,
 				'position': 'fixed'
 			}); 
-			
 			numFrames = numFrames + 1;
-
-			/*
-			// DEBUG TEXT
-			$('.debug').text('DEBUG pos: '+x+', '+y+'; '+
-					'click: '+currentMousePos.x+', '+currentMousePos.y+'; '+ 
-					newFrame.width() +', '+ newFrame.height() + ', ' + $(".sketch").children().length +
-					', ' + numFrames
-			);*/
 		} //end else
 	});  
 
 
 	// Start Dragging Frame
-	var dragging = null; // Pointer to Frame being Dragged
 	$(document).on("mousedown",".selected",function(e) {
-		if(dragging==null){ // Only Drag One Frame At A Time
-			var dragging=$(this);	//to prevent other elements from being moved
-			var position = $(this).offset();
-			dragging.css('cursor','move');
-		
-			$(document).on("mousemove",function(event){
+		var dragging=$(this);	//to prevent other elements from being moved
+		var position = $(this).offset();
+		dragging.css('cursor','move');
+	
+		$(document).on("mousemove",function(event){
 
-				var move_left = position.left + event.pageX - e.pageX;
-				var move_top = position.top + event.pageY - e.pageY;
+			var move_left = position.left + event.pageX - e.pageX;
+			var move_top = position.top + event.pageY - e.pageY;
 
-				//Set new position
-				dragging.parent().css({	//only moves what is being dragged, not others by accident.
-					'left': move_left,
-					'top': 	move_top,
-				}); 
-				//Calculate and set new Position from Centre
-				var posX = move_left - screen_centre.x + window_pos.x;
-				var posY = move_top - screen_centre.y + window_pos.y;
-				dragging.parent().attr({
-					'data-centre-x': posX,
-					'data-centre-y': posY
-				});
-
-				// DEBUG TEXT
-				$('.debug').text('DEBUG pos: '+position.left+', '+position.top+'; '+
-					'click: '+e.pageX+', '+e.pageY+'; '+
-					'current: '+event.pageX+', '+event.pageY+'; '+
-					'new_pos: '+move_left+', '+move_top	+'; '+
-					dragging.parent().attr('id') 
-				);
+			//Set new position
+			dragging.parent().css({	//only moves what is being dragged, not others by accident.
+				'left': move_left,
+				'top': 	move_top,
+			}); 
+			//Calculate and set new Position from Centre
+			var posX = move_left - screen_centre.x + window_pos.x;
+			var posY = move_top - screen_centre.y + window_pos.y;
+			dragging.parent().attr({
+				'data-centre-x': posX,
+				'data-centre-y': posY
 			});
-		} //if(dragging==null)	
+
+			// DEBUG TEXT
+			$('.debug').text('DEBUG pos: '+position.left+', '+position.top+'; '+
+				'click: '+e.pageX+', '+e.pageY+'; '+
+				'current: '+event.pageX+', '+event.pageY+'; '+
+				'new_pos: '+move_left+', '+move_top	+'; '+
+				dragging.parent().attr('id') 
+			);
+		});	
 	});
 
 	// Stop Dragging Frame
 	$(document).on("mouseup",function(e) {
 		$(document).off("mousemove");
-		if(dragging!=null){
-			dragging.css('cursor','auto');
-			//$(this).css('cursor','auto');
-		}
-		dragging = null;
-		$('.debug').text('DEBUG '+dragging);
+		$(".selected").css('cursor','auto');
 	});	
 
-	// BUTTON EVENTS
-
-	// ADD BUTTON
-	$(".addbtn").click(function(){
-		remove = "false";
-		$(".sketch").css('cursor', 'auto');
-		$(".container").css('cursor', 'auto');
-		var num = "id"+$(".sketch").children().length;
-		jQuery("<div/>", {
-		    id: num,
-		}).appendTo(".sketch");
-		$("#"+num).load("box.htm");
-	});
-
-	// REMOVE BUTTON
-	var remove = "false";
-	$(".rembtn").click(function(){
-		if(remove=="false"){
-			remove = "true";
-			$(".sketch").css('cursor', 'crosshair');
-			$(".container").css('cursor', 'crosshair');
-		} else {
-			remove = "false";
-			$(".sketch").css('cursor', 'auto');
-			$(".container").css('cursor', 'auto');
-		}
-		//var num = "id"+($(".sketch").children().length-1);
-		//$("#"+num).remove();
-	});
+	
 });
